fc788e80589414c45ef910e51db8f7264cec3588
Sebastian Castillo <145416890+SebasRaul2503@users.noreply.github.com>
2025-06-10 23:04:49 -0500

    Update cultura.php

--- VERCEL/api/secciones/cultura.php
+++ VERCEL/api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "asdasdsadsad",
+"autor" => "asdsadas",
+"resumen" => "asdasdasdasd",
 ];
 ?>
